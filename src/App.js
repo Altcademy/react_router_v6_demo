443109d23c92901a22ad17c51d7351744cceefc2
David Young <xiaoyang88@gmail.com>
2023-03-06 15:23:04 +0800

    basic react router v6 app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const NavBar = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>React Rotuer v6 Demo</h2>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="press">Press</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+const Home = function () {
+  return <h1>Home</h1>
+}
+
+const Press = function () {
+  return <h1>Press</h1>
+}
+
+const App = function () {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="press" element={<Press />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
